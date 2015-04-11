bcd1f3e0caec4acfdabe3669153feeee5925f285
dhnishi <dhnishi@gmail.com>
2015-04-11 13:26:52 -0700

    Add initial Typescript definitions and working Karma suite.

---
+++ src/test/testspec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by dhnishi on 4/11/15.
+ */
+/// <reference path="../../typings/jasmine/jasmine.d.ts"/>
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        expect(true).toBe(true);
+    });
+});
+//# sourceMappingURL=testspec.js.map
---
+++ src/test/testspec.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by dhnishi on 4/11/15.
+ */
+
+/// <reference path="../../typings/jasmine/jasmine.d.ts"/>
+
+describe("A suite", () => {
+    it("contains spec with an expectation", () => {
+        expect(true).toBe(true);
+    });
+});
