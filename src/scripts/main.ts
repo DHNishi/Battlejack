b3bc408a4d8dd2cbab0f04de0133533cc79a403f
dhnishi <dhnishi@gmail.com>
2015-04-12 17:07:50 -0700

    Begin adding Angular components for the views.

---
+++ src/scripts/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by dhnishi on 4/12/15.
+ */
+
+/// <reference path="../../typings/angularjs/angular.d.ts"/>
+
+var app = angular.module('blackjackApp', []);
---
+++ src/scripts/services/BattleEntitiesService.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by dhnishi on 4/12/15.
+ */
+
+/// <reference path="../../../typings/angularjs/angular.d.ts"/>
+/// <reference path="../Entity.ts"/>
+/// <reference path="../main.ts"/>
+
+app.factory('battleEntitiesService', makeBattleEntitiesService);
+
+function makeBattleEntitiesService() {
+    console.log("service made");
+    // TODO: Maybe turn this into its own class?
+    var service : any = {};
+
+    // We store the entities in the service itself.
+    service.entities = [];
+
+    service.clearEntities = () => {
+        service.entities = [];
+    };
+
+    // Adds a new entity in battle mode.
+    service.addEntity = (entity : Entity) => {
+        var newEntity = new EntityInBattle(entity, undefined);
+        service.entities.push(newEntity);
+    };
+
+    return service;
+}
